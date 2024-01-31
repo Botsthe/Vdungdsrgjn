95047f2a41d050adf973cb72d405fc5d302ceddb
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2024-01-31 17:34:16 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,139 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+
+class script(object):
+    START_TXT = """<b>Hᴇʟʟᴏ {},ᴍʏ ɴᴀᴍᴇ {},I ᴀᴍ ᴀ ᴘᴏᴡᴇʀғᴜʟ ғɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ ᴡɪᴛʜ Cʟᴏɴᴇ + Sᴛʀᴇᴀᴍ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ + Cᴜꜱᴛᴏᴍ Uʀʟ ꜱʜᴏʀᴛɴᴇʀ ғᴇᴀᴛᴜʀᴇ + ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ғᴇᴀᴛᴜʀᴇ\n▪Mᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ: <a href="https://t.me/Ek4mpreetSingh">Eᴋᴀᴍᴘʀᴇᴇᴛ Sɪɴɢʜ</a></b>"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+    
+    CAPTION = """<b>▫️ ғɪʟᴇɴᴀᴍᴇ : {file_name}
+
+▫️ sɪᴢᴇ : {file_size}
+</b>""" 
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+    SHORTENER_API_MESSAGE = """<b>Tᴏ ᴀᴅᴅ ᴏʀ ᴜᴘᴅᴀᴛᴇ ʏᴏᴜʀ Sʜᴏʀᴛɴᴇʀ Wᴇʙsɪᴛᴇ API, /api (ᴀᴘɪ)
+            
+<b>Ex: /api 𝟼LZǫ𝟾𝟻𝟷sXᴏғғғPHᴜɢɪKQǫ
+
+<b>Cᴜʀʀᴇɴᴛ Wᴇʙsɪᴛᴇ: {base_site}
+
+Cᴜʀʀᴇɴᴛ Sʜᴏʀᴛᴇɴᴇʀ API:</b> `{shortener_api}`"""
+
+
+    CLONE_START_TXT = """<b>Hᴇʟʟᴏ {},ᴍʏ ɴᴀᴍᴇ {},I ᴀᴍ ᴀ ᴘᴏᴡᴇʀғᴜʟ ғɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ ᴡɪᴛʜ Cʟᴏɴᴇ + Sᴛʀᴇᴀᴍ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ + Cᴜꜱᴛᴏᴍ Uʀʟ ꜱʜᴏʀᴛɴᴇʀ ғᴇᴀᴛᴜʀᴇ + ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ғᴇᴀᴛᴜʀᴇ</b>"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+    ABOUT_TXT = """<b>◁ ᴍʏ ɴᴀᴍᴇ: {}
+◁ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/EK4MPREETSINGH>Eᴋᴀᴍᴘʀᴇᴇᴛ Sɪɴɢʜ</a>
+◁ ʟᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pʏᴛʜᴏɴ 𝟹</a>
+◁ ʟɪʙʀᴀʀʏ: <a href=https://docs.pyrogram.org>Pʏʀᴏɢʀᴀᴍ</a>
+◁ ᴅᴀᴛᴀʙᴀꜱᴇ : <a href=https://docs.mongodb.com>ᴍᴏɴɢᴏDB</a>
+◁ Sᴇʀᴠᴇʀ : <a href="https://render.com">ʀᴇɴᴅᴇʀ</a>
+◁ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ : ᴠ.2.7.1 [ꜱᴛᴀʙʟᴇ]</b>
+"""
+
+    OWNER_TXT = """◈ ɴᴀᴍᴇ : Eᴋᴀᴍᴘʀᴇᴇᴛ Sɪɴɢʜ
+◈ ᴜꜱᴇʀɴᴀᴍᴇ : @EK4MPREETSINGH
+◈ ᴘᴇʀᴍᴀɴᴇɴᴛ ᴅᴍ ʟɪɴᴋ : <a href='https://t.me/EK4MPREETSINGH'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>"""
+
+    SOURCE_TXT = """Tʜɪꜱ ʙᴏᴛ ɪꜱ ᴀ ᴄʟᴏꜱᴇᴅ ꜱᴏᴜʀᴄᴇ ᴘʀᴏɪᴇᴄᴛ.
+Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʙᴜʏ ᴍʏ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʏᴏᴜ ᴄᴀɴ ᴄᴏɴᴛᴀᴄᴛ ᴍʏ <a href='https://t.me/EK4MPREETSINGH'>ᴏᴡɴᴇʀ</a>"""
+
+    DISCLAIMER_TXT = """Yᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ ᴛᴏ ꜱᴛᴏʀᴇ ᴀɴʏ ᴛʏᴘᴇ ᴏғ ғɪʟᴇ, ʙᴜᴛ ᴡᴇ ꜱᴛʀɪᴄᴛʟʏ ʀᴇǫᴜᴇꜱᴛ ʏᴏᴜ ɴᴏᴛ ᴛᴏ ꜱᴀᴠᴇ ᴀɴʏ ᴘᴏʀɴᴏɢʀᴀᴘʜɪᴄᴀʟ ᴄᴏɴᴛᴇɴᴛ. Oᴛʜᴇʀᴡɪꜱᴇ ᴡᴇ ʜᴀᴠᴇ ᴛᴏ ᴡᴇ ʀᴇᴍᴏᴠᴇ ɪᴛ ғʀᴏᴍ ʙᴏᴛ.
+
+    Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ: <a href="https://t.me/EK4MPREETSINGH">Eᴋᴀᴍᴘʀᴇᴇᴛ Sɪɴɢʜ</a>"""
+
+    CABOUT_TXT = """<b>◁ ᴍʏ ɴᴀᴍᴇ: {}
+◁ ᴘᴀʀᴇɴᴛ ʙᴏᴛ : <a href=https://t.me/Filestore_Galaxy_bot>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
+◁ ʟᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pʏᴛʜᴏɴ 𝟹</a>
+◁ ʟɪʙʀᴀʀʏ: <a href=https://docs.pyrogram.org>Pʏʀᴏɢʀᴀᴍ</a>
+◁ ᴅᴀᴛᴀʙᴀꜱᴇ : <a href=https://docs.mongodb.com>ᴍᴏɴɢᴏDB</a>
+◁ Sᴇʀᴠᴇʀ : <a href="https://cloud.google.com">ᴠᴘꜱ</a>
+◁ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ : ᴠ.2.7.1 [ꜱᴛᴀʙʟᴇ]</b>
+"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+    CLONE_TXT = """<b>ʜᴇʟʟᴏ {}
+ғᴏʟʟᴏᴡ ᴛʜᴇꜱᴇ ꜱᴛᴇᴘꜱ ᴛᴏ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ғɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ:
+
+1) sᴇɴᴅ <code>/newbot</code> ᴛᴏ @BotFather
+2) ɢɪᴠᴇ ᴀ ɴᴀᴍᴇ ꜰᴏʀ ʏᴏᴜʀ ʙᴏᴛ.
+3) ɢɪᴠᴇ ᴀ ᴜɴɪǫᴜᴇ ᴜsᴇʀɴᴀᴍᴇ.
+4) ᴛʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ʏᴏᴜʀ ʙᴏᴛ ᴛᴏᴋᴇɴ.
+5) ꜰᴏʀᴡᴀʀᴅ ᴛʜᴀᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴇ.
+</b>"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+    HELP_TXT = """<b>Cʟɪᴄᴋ ᴀɴʏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴄᴏᴍᴍᴀɴᴅꜱ</b>"""
+
+    FILESTORE_TXT = """<b>🍀Hᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅꜱ:
+    
+• /link - ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴠɪᴅᴇᴏ ᴏʀ ғɪʟᴇ ᴛᴏ ɢᴇᴛ sʜᴀʀᴀʙʟᴇ ʟɪɴᴋ
+
+• /batch - sᴇɴᴅ ғɪʀsᴛ ʟɪɴᴋ ᴏғ ғɪʟᴇ sᴛᴏʀᴇ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛ ᴛʜᴇɴ ʟᴀsᴛ ᴘᴏsᴛ ʟɪɴᴋ ᴀɴᴅ ᴍᴀᴋᴇ sᴜʀᴇ ʙᴏᴛ ɪs ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ғɪʟᴇ sᴛᴏʀᴇ ᴄʜᴀɴɴᴇʟ.
+ᴇx - /batch <code>https://t.me/MoviesGalaxy01/25 https://t.me/Moviesgalaxy01/30</b>"""
+
+    SHORTLINK_TXT = """<b>🍀Hᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅꜱ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ꜱʜᴏʀᴛʟɪɴᴋ:
+    
+• /base_site - ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ sᴇᴛ ᴜʀʟ sʜᴏʀᴛɴᴇʀ ʟɪɴᴋ ᴅᴏᴍᴀɪɴ 
+ᴇx - /base_site shortlinksite.com
+
+• /api - sᴇᴛ ʏᴏᴜʀ ᴜʀʟ sʜᴏʀᴛɴᴇʀ ᴀᴄᴄᴏᴜɴᴛ ᴀᴘɪ 
+ᴇx - /api d7f9wh9wuw73ij03j36bdtr5fhd0</b>"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+
+    CHELP_TXT = """<b>🍀Hᴇʀᴇ ᴀʀᴇ ᴛʜᴇ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ:
+
+• /link - ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴠɪᴅᴇᴏ ᴏʀ ғɪʟᴇ ᴛᴏ ɢᴇᴛ sʜᴀʀᴀʙʟᴇ ʟɪɴᴋ
+
+• /base_site - ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ sᴇᴛ ᴜʀʟ sʜᴏʀᴛɴᴇʀ ʟɪɴᴋ ᴅᴏᴍᴀɪɴ
+ᴇx - /base_site yoursite.com
+
+• /api - sᴇᴛ ʏᴏᴜʀ ᴜʀʟ sʜᴏʀᴛɴᴇʀ ᴀᴄᴄᴏᴜɴᴛ ᴀᴘɪ
+ᴇx - /api h8b786v76c68h89h756b7hn876g6r67vr56vu6rg
+
+• /broadcast - ʀᴇᴘʟʏ ᴛᴏ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ (ʙᴏᴛ ᴏᴡɴᴇʀ ᴏɴʟʏ)</b>"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+    LOG_TEXT = """<b>#NewUser
+    
+ID - <code>{}</code>
+
+Nᴀᴍᴇ - {}</b>
+"""
+    RESTART_TXT = """
+<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
+
+📅 Dᴀᴛᴇ : <code>{}</code>
+⏰ Tɪᴍᴇ : <code>{}</code>
+🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
---
+++ config.py
@@ -0,0 +1,106 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+
+import re
+import os
+from os import getenv, environ
+from Script import script 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+      
+# Owner Information
+API_ID = int(environ.get("API_ID", "25705219"))
+API_HASH = environ.get("API_HASH", "6590905e28c61bca1ad5e83de9853cf8")
+ADMINS = int(environ.get("ADMINS", "5022283560"))
+
+# Database Information
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://CLONEBOT:CLONEBOT@cluster0.g2yiuvi.mongodb.net/?retryWrites=true&w=majority")
+CDB_NAME = environ.get("CDB_NAME", "cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://FILESTOREBOT:FILESTOREBOT@cluster0.enstfjz.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = environ.get("DB_NAME", "cluster0")
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# Bot Information
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_USERNAME = environ.get("BOT_USERNAME", "Filestore_Galaxy_bot") # your bot username without @
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/7e3b2b02d5bd88fffd0ed.jpg https://te.legra.ph/file/ada4e7758a4164ee0b5b0.jpg https://te.legra.ph/file/6f2877bb1e183bcf3979e.jpg https://te.legra.ph/file/52423c13fe3ef49ae465d.jpg https://te.legra.ph/file/985d471fe1416399d5248.jpg https://te.legra.ph/file/eaf6e3e350308fc5d53d0.jpg')).split() # Bot Start Picture
+
+# Auto Delete Information
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+
+# Channel Information
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002100922885"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002100654462')).split()]
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# File Caption Information
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+
+# Enable - True or Disable - False
+PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# File Stream Config
+class Var(object):
+    MULTI_CLIENT = False
+    name = str(getenv('name', 'galaxyfilestore'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002107444913'))
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = None
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME'))
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',True))
+    if HAS_SSL:
+        URL = "https://galaxyfilestore.onrender.com"
+    else:
+        URL = "https://galaxyfilestore.onrender.com"
+
+
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+    
